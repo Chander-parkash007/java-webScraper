fcb23c44641398579ae33ed0d0897ed4d677d6e6
Chander Parkash <khatriiiranjhi12@gmail.com>
2026-03-11 23:09:13 +0500

    fixing service class and adding useragent+response timeout

--- src/main/java/com/Webscrapping/Controller/Scrapcontroller.java
+++ src/main/java/com/Webscrapping/Controller/Scrapcontroller.java
@@ -3,12 +3,12 @@ package com.Webscrapping.Controller;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.Webscrapping.Model.ScrapResponse;
 import com.Webscrapping.Service.ScraperService;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class Scrapcontroller {
